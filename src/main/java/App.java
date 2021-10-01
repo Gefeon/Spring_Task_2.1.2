707d00c0144cc85e60fb54f939df09e18191d83a
Pavel Zuev <gefeon@gmail.com>
2021-10-01 21:54:36 +0300

    Initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,31 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
+
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        boolean compareHelloWorld = bean1 == bean2;
+        System.out.println(compareHelloWorld);
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getName());
+
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getName());
+
+        boolean compareCat = bean3 == bean4;
+        System.out.println(compareCat);
+
     }
 }
